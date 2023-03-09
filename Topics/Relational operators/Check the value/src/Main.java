a7acbadd247869999e5ba9b3b70faaaeb868af28
oldhats <112814788+oldhats@users.noreply.github.com>
2023-03-09 01:33:43 -0500

    Initial commit

---
+++ Topics/Relational operators/Check the value/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        //System.out.println("Enter a number: ");
+        int userNum = scanner.nextInt();
+
+        System.out.println(10 > userNum);
+
+    }
+}
